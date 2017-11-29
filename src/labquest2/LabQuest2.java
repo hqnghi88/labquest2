d78f0e05339c82c2ec1ff4816a0b882131054c8b
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-11-29 21:43:12 +0700

    try to fix travis, sorry for the spam mails ci deploy

--- src/labquest2/LabQuest2.java
+++ src/labquest2/LabQuest2.java
@@ -103,7 +103,7 @@ public class LabQuest2 {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(connectButton.getText().equals("Connect")){
-				 arduino = new Arduino(portList.getSelectedItem().toString(),4800);
+				 arduino = new Arduino(portList.getSelectedItem().toString(),9600);
 				 if(arduino.openConnection()){
 					 connectButton.setText("Disconnect");
 					 portList.setEnabled(false);
